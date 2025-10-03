13b647bed35580c41b3c80a84b027ab37056f100
ozan1535 <ozanbilgic1535@gmail.com>
2025-10-03 13:15:10 +0300

    Style footer to keep on the bottom

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Footer() {
   const { language } = useLanguage();
   return (
-    <div className="w-full h-10 bg-blue-400 flex justify-center items-center">
+    <div className="w-full h-10 bg-blue-400 flex justify-center items-center absolute bottom-0">
       {language === "tr" ? "Tüm hakları saklıdır" : "All rights are reserved"}
     </div>
   );
