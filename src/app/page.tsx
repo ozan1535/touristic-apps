ed8de12b931661ca0d7b743a7ac558c3a7f9dba0
ozan1535 <ozanbilgic1535@gmail.com>
2025-10-20 17:20:28 +0300

    Add new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AppDetails from "@/components/Dialog/AppDetails";
 import WorldMap from "@/components/NoSsr/WorldMap/WorldMap";
+import RotatingWorldChart from "@/components/RotatingWorldChart/RotatingWorldChart";
 import { SearchCountry } from "@/components/SearchCountry/SearchCountry";
 import { Metadata } from "next";
 
@@ -13,9 +14,21 @@ export default function Home() {
   return (
     <div className="h-[90vh] w-full p-10 ">
       <AppDetails />
-      <SearchCountry />
+      <h1 className="text-3xl md:text-5xl font-black text-center text-purple-primary">
+        GlobalAppGuide
+      </h1>
+
+      <p className="text-center text-secondary my-3 md:my-8">
+        Your ultimate companion for navigating life abroad. Discover essential
+        apps and local prices for any country.
+      </p>
+      <div className="flex justify-center items-center w-full text-center">
+        <div className="w-full md:w-[50%]">
+          <SearchCountry />
+        </div>
+      </div>
       <div className="flex justify-center mt-10">
-        <WorldMap />
+        <RotatingWorldChart />
       </div>
     </div>
   );
