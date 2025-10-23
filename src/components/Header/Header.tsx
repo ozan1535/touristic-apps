64a837012537154f5440366bd13e58ba4a058ed5
ozan1535 <ozanbilgic1535@gmail.com>
2025-10-23 13:49:35 +0300

    Add a mock header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,33 @@ import SelectLanguage from "../SelectLanguage/SelectLanguage";
 function Header() {
   return (
     <div className="w-full h-12 bg-primary border-b flex items-center justify-between px-10">
-      <div>
+      <div className="flex gap-5">
         <Link href={"/"} className="text-secondary font-black">
           GlobalAppGuide
         </Link>
+        <div className="flex gap-3">
+          <Link
+            href={"/ai-travel-planner"}
+            className="text-secondary"
+          >
+            AI Planner
+          </Link>
+          <Link
+            href={"/cultural-insights"}
+            className="text-secondary"
+          >
+            Cultural Insights
+          </Link>
+          <Link
+            href={"/share-your-knowledge"}
+            className="text-secondary"
+          >
+            Contribute
+          </Link>
+          <Link href={"/user/username"} className="text-secondary">
+            Profile
+          </Link>
+        </div>
       </div>
       <div>
         <SelectLanguage />
