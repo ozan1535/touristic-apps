246e28de20f25f87606f58e0e2c8bb8d32a94201
ozan1535 <ozanbilgic1535@gmail.com>
2025-12-01 16:13:23 +0300

    Add affiliate banner

---
+++ src/components/AffiliateBanner/AffiliateBanner.tsx
@@ -0,0 +1,29 @@
+import { ChevronRight, Wifi } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+function AffiliateBanner() {
+  return (
+    <div className="w-full px-4 md:px-6 lg:px-10 pt-10">
+      <div className="max-w-7xl mx-auto">
+        <Link
+          href={"#12312"}
+          className="w-full max-w-3xl mx-auto bg-gradient-to-r from-amber-700 to-yellow-500 p-4 rounded-2xl flex items-center gap-4 shadow-sm"
+        >
+          <div className="bg-white p-2 rounded-xl">
+            <Wifi size={20} color="blue" />
+          </div>
+          <div className="flex-1">
+            <h4 className="text-white font-bold text-sm">Don't stay offline</h4>
+            <p className="text-white text-xs font-medium mt-0.5 leading-tight">
+              Get 15% off with Airalo (Code: IST15)
+            </p>
+          </div>
+          <ChevronRight className="text-white" size={20} />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default AffiliateBanner;
