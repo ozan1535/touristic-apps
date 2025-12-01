2e7e627fc2e44c19133e048df7ed2aa187bc51a3
ozan1535 <ozanbilgic1535@gmail.com>
2025-12-01 16:11:02 +0300

    Allow unsplash pictures

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "qmeplsipesyoodcwjotf.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
