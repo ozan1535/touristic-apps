2b8ffa6b3bcbd276a074019c3682c46558f1f2e8
ozan1535 <ozanbilgic1535@gmail.com>
2025-12-06 17:52:37 +0300

    change style of bottom nav bar

--- src/components/Mobile/MobileBottomNav/MobileBottomNav.tsx
+++ src/components/Mobile/MobileBottomNav/MobileBottomNav.tsx
@@ -3,28 +3,16 @@ import { getTabs } from "@/components/Footer/Footer.helpers";
 import React from "react";
 import CenterTabButton from "./CenterTabButton";
 import NavTabButton from "./NavTabButton";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 
 function MobileBottomNav({ profile }) {
   const path = usePathname();
-
+  const { locale } = useParams();
   return (
     <div className="md:hidden w-full fixed bottom-1 bg-gradient-to-r from-blue-400 to-indigo-600 backdrop-blur-xl border border-slate-200 dark:border-slate-700 shadow-2xl rounded-3xl p-2 flex justify-between items-center z-50 safe-area-bottom">
-      {getTabs(profile).map((tab) =>
-        tab.center ? (
-          <CenterTabButton
-            key={tab.id}
-            tab={tab}
-            active={path.includes(tab.href)}
-          />
-        ) : (
-          <NavTabButton
-            key={tab.id}
-            tab={tab}
-            active={path.includes(tab.href)}
-          />
-        )
-      )}
+      {getTabs(profile, locale).map((tab) => (
+        <NavTabButton key={tab.id} tab={tab} active={path.includes(tab.href)} />
+      ))}
     </div>
   );
 }
